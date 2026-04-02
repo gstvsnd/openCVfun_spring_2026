927bd9a808d532b0010d1e4e7b6c8fc8ac4d8f39
gstvsnd <mail@gustavsand.se>
2026-04-02 14:41:36 +0200

    Force artist to lable and save each drawing

--- main.py
+++ main.py
@@ -29,8 +29,7 @@ def mouse_listener(event, x, y, f, p):
 # Initialize canvas
 canvas = np.zeros((512, 512, 1), np.uint8)
 canvas.fill(255) # White background
-drawing = False # Decides if mouse is painting
-print("Space - skip\nEnter - save")
+print("Draw a Square, circle or triangle\nSpace  - clear\nEnter  - save\nEscape - Finish drawing")
 
 # Create window and set mouse callback
 cv2.namedWindow("canvasWindow")
@@ -39,11 +38,14 @@ cv2.setMouseCallback("canvasWindow", mouse_listener)
 
 # Program states:
 STATE_COLLECT = 0
-STATE_TRAIN   = 1
-STATE_PREDICT = 2
+STATE_LABEL   = 1
+STATE_TRAIN   = 2
+STATE_PREDICT = 3
 current_STATE = STATE_COLLECT # Initial state
 
+# Global variables:
 index = 1 # Counts drawings
+drawing = False # Decides if mouse is painting
 
 while True:
 
@@ -52,13 +54,36 @@ while True:
         cv2.imshow("canvasWindow", canvas)
         key = cv2.waitKey(1) & 0xFF
         if key == 32: # Space
-            canvas.fill(255) # Clear drawing
+            canvas.fill(255) # Clear drawing (retry)
         elif key == 13: # Enter
-            cv2.imwrite("drawings/drawing_" + str(index) + ".png", canvas) # Saves drawing under drawings folder
-            index += 1
-            canvas.fill(255) # Clear drawing
+            # Define label for drawing and save drawing
+            print("What have you drawn?\n1 - Square\n2 - Circle\n3 - Triangle")
+            current_STATE = STATE_LABEL # Switch state to label drawing
         elif key == 27: # Escape
             current_STATE = STATE_TRAIN # Escapes state
+
+    # Label drawings
+    elif current_STATE == STATE_LABEL:
+        # Force the artist to take a brak and label the drawing
+        cv2.moveWindow("canvasWindow", 5000, 5000) # Moves the window in a weird way (lower right corner of my screen)
+        label_key = cv2.waitKey(1) & 0xFF
+        label = "undeclared"
+        if label_key == 49: # '1'
+            label = "square"
+        elif label_key == 50: # '2'
+            label = "circle"
+        elif label_key == 51: # '3'
+            label = "triangle"
+        elif label_key == 27 or label_key == 13: # Escape or Enter
+            label = "other"
+        else:
+            pass
+        if label != "undeclared":
+            cv2.imwrite("drawings/drawing_" + str(index) + "_ " + label + ".png", canvas) # Saves drawing under drawings folder
+            index += 1
+            canvas.fill(255) # Clear drawing
+            cv2.moveWindow("canvasWindow", 100, 100) # Moves "back" the window
+            current_STATE = STATE_COLLECT # Continue collecting drawings
 
     # Train AI
     elif current_STATE == STATE_TRAIN:
