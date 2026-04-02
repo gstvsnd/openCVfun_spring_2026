94118df79c29ecb24bcc6d6a33008e0133fcdbbc
gstvsnd <mail@gustavsand.se>
2026-04-02 13:26:28 +0200

    Ussr can draw multiple drawings and mainfile is nicer structured

--- main.py
+++ main.py
@@ -8,12 +8,6 @@ import numpy as np
 # Draw a number or something using a paint popup window to create training data
 # Use some AI to learn a python program to identify new drawing based on the training data
 
-# Initialize canvas
-canvas = np.zeros((512, 512, 1), np.uint8)
-canvas.fill(255) # White background
-drawing = False # Decides if mouse is painting
-print("Space - skip\nEnter - save")
-
 def mouse_listener(event, x, y, f, p):
     # 'event'   - mouse event type
     # 'x' & 'y' - coordinates
@@ -31,31 +25,34 @@ def mouse_listener(event, x, y, f, p):
         if drawing == True:
             canvas[y-n:y+n, x-n:x+n] = 0 # Draw a small square
 
+# Initialize canvas
+canvas = np.zeros((512, 512, 1), np.uint8)
+canvas.fill(255) # White background
+drawing = False # Decides if mouse is painting
+print("Space - skip\nEnter - save")
 
 # Create window and set mouse callback
 cv2.namedWindow("canvasWindow")
 cv2.setMouseCallback("canvasWindow", mouse_listener)
 
-# Program loop
+
+# Collect drawings: 
+index = 1
 while True:
     cv2.imshow("canvasWindow", canvas)
     key = cv2.waitKey(1) & 0xFF
-    
-    # TODO: Collect drawings 
-    index = 1
-    while True:
-        if key == 32: # Space
-            canvas.fill(255) # Clear drawing
-        elif key == 13: # Enter
-            cv2.imwrite("drawings/drawing1.png", canvas) # Saves immage under drawings folder
-            canvas.fill(255) # Clear drawing
-        elif key == 27: # Escape
-            break
-    break # Escape matrix
+    if key == 32: # Space
+        canvas.fill(255) # Clear drawing
+    elif key == 13: # Enter
+        cv2.imwrite("drawings/drawing_" + str(index) + ".png", canvas) # Saves drawing under drawings folder
+        index += 1
+        canvas.fill(255) # Clear drawing
+    elif key == 27: # Escape
+        break # Escape matrix
 
-    # TODO: Train AI to recognize drawings
+# TODO: Train AI to recognize drawings
 
-    # TODO: Use AI to identify new drawings
+# TODO: Use AI to identify new drawings
     
 
 cv2.destroyAllWindows()
